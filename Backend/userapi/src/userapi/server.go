099dc1e838c955c93b8f5e02692c1cb1845f4403
Raviteja <raviteja.kommalapati@sjsu.edu>
2019-04-08 03:49:58 -0700

    Created Register User Service
    Created User Registration Service

--- Backend/userapi/src/userapi/server.go
+++ Backend/userapi/src/userapi/server.go
@@ -1,22 +1,55 @@
 package main
 
+import (
+	"net/http"
 
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+)
 
+//MongoDB Configuration
+var mongodb_server = "mongodb://127.0.0.1:27017"
+var mongodb_database = "cmpe281"
+var user_collection = "user"
+var userregistration_collection = "userregistration"
 
+func NewServer() *negroni.Negroni {
+	formatter := render.New(render.Options{
+		IndentJSON: true,
+	})
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+	initRoutes(mx, formatter)
+	n.UseHandler(mx)
+	return n
+}
+
+func initRoutes(mx *mux.Router, formatter *render.Render) {
+	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/users", pingHandler(formatter)).Methods("POST")
+
+}
+
+// API Ping Handler
+func pingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"API version 1.0 alive!"})
+	}
+}
 
 //API New User Registration
+func userRegistrationHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
 
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"API version 1.0 alive!"})
+	}
+}
 
 //Get User
 
-
-
 //APISend and Resend Confirmation Email
 
-
-
 //Update password
-
-
 
 //Update Name
