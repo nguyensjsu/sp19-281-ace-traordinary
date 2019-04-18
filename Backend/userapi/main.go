49f16b44e813b023dc920a0fcb57f7b7658c595c
Raviteja <raviteja.kommalapati@sjsu.edu>
2019-04-18 13:11:17 -0700

    Changes to the Mongo Db
    Tried Different Cloud paltform connection ended up using AWS personel inastance

--- Backend/userapi/main.go
+++ Backend/userapi/main.go
@@ -11,11 +11,11 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/users", handlers.CreateUserEndpoint).Methods("GET")
-	router.HandleFunc("/users/{id}", handlers.GetPersonEndpoint).Methods("GET")
-	router.HandleFunc("/users", handlers.CreatePersonEndpoint).Methods("POST")
-	router.HandleFunc("/users", handlers.DeletePersonEndpoint).Methods("DELETE")
-	router.HandleFunc("/users/{id}", handlers.UpdateUserEndpoint).Methods("PUT")
+	//router.HandleFunc("/users", handlers.GetUserEndpoint).Methods("GET")
+	router.HandleFunc("/users", handlers.RegisterUserEndpoint).Methods("POST")
+	//router.HandleFunc("/users", handlers.CreatePersonEndpoint).Methods("POST")
+	//router.HandleFunc("/users", handlers.DeletePersonEndpoint).Methods("DELETE")
+	//router.HandleFunc("/users/{id}", handlers.UpdateUserEndpoint).Methods("PUT")
 	fmt.Println("Starting server on port 8000...")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
